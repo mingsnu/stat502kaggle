9a150f0d863bee7bf26dd23a61e2206c1f86dba5
weich <weich@iastate.edu>
2016-04-29 02:18:33 -0500

    hexiehao

--- User/weicheng/03_xgboost_hexiehao.R
+++ User/weicheng/03_xgboost_hexiehao.R
@@ -1,12 +1,9 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-m = as.numeric(args[1])
-R = as.numeric(args[2])
-if(is.na(m))
-  m = 20
+R = as.numeric(args[1])
 if(is.na(R))
   R = 5
-cat("m: ", m, " R: ", R, "\n")
+cat("R: ", R, "\n")
 library(Matrix)
 library(xgboost)
 library(caTools)
@@ -14,6 +11,7 @@ library(caret)
 library(readr)
 library(dplyr)
 library(tidyr)
+library(data.table)
 
 ### Loading data & features
 trn = readRDS("train_clean.RDS")
@@ -22,55 +20,83 @@ ftrn = read.csv("../../feature/feature_all_train_ratio_only_wc_99.csv")
 #ftst = read.csv("../../feature/feature_all_test_ratio_only_wc_99.csv")
 trn = left_join(trn, ftrn)
 #tst = left_join(tst, ftst)
+trn = data.table(trn)
+#tst = data.table(tst)
 
 trn.y = trn$TARGET
 impVars = readRDS("impVars.rds")
+impVars = setdiff(impVars, c("var15_ratio", "var38_ratio"))
 
-#### stratified sampling for train
+#### stratified sampling for training & testing
 set.seed(20160428)
-trn.idx = createDataPartition(y = y, times = R, p = .75)
+trn.idx = createDataPartition(y = trn.y, times = R, p = .75)
 
-############### 
-## Number of feature to use
-N = 1000
-M = length(impVars)
-optpar = data.frame(Rounds=2000, Depth = 4, r_sample = 0.8, eta =0.01,
-                    best_round = 774 )
-res = data.frame(matrix(ncol=m+R, nrow=0))
-names(res) = c(paste0("f", 1:m), paste0("auc", 1:R))
-for(i in 1:N){
-  ftr = impVars[sample(1:M, m, prob = M:1)]
-  ftr.idx = names(trn) %in% ftr
-  score = c()
-  for(j in 1:R){
-    training = trn[trn.idx[[j]], ftr.idx]
-    training.y = trn.y[trn.idx[[j]]]
-    testing = trn[-trn.idx[[j]], ftr.idx]
-    testing.y = trn.y[-trn.idx[[j]]]
-    
-    dtrain <- xgb.DMatrix(data=Matrix(as.matrix(training), sparse = TRUE), label=training.y, missing = -999)
-    dtest <- xgb.DMatrix(data=Matrix(as.matrix(testing), sparse = TRUE), label=testing.y, missing = -999)
-    watchlist <- list(test=dtest)
-    
-    print("Train xgboost using xgb.train with watchlist")
-    params = list(
-      objective = "binary:logistic",    # binary classification
-      eta = optpar$eta,       # learning rate
-      max_depth = optpar$Depth,      # max tree depth
-      subsample = optpar$r_sample,
-      eval_metric = "auc"     # evaluation/auc metric
-    )
-    bst <- xgb.train(params = params,
-                     data=dtrain,
-                     nrounds = 2000,
-                     watchlist=watchlist,
-                     verbose = 1,
-                     early.stop.round = 20)
-    if(bst$bestScore < 0.8) next
-    score = c(score, bst$bestScore)
+####
+optpar = data.frame(Rounds=2000, Depth = 4, r_sample = 0.8, eta =0.01)
+## initial feature. fixed.
+ftrSelected = c("saldo_var30", "var15", "var38")
+aucMean = c(0, 0, 0)
+aucStd = c(0, 0, 0)
+res = data.table(ftrSelected, aucMean, aucStd)
+k = 1
+while(1){
+  k = k + 1
+  cat("k: ", k, "\n")
+  impVars = setdiff(impVars, ftrSelected)
+  if(length(impVars) == 0) stop("Can't believe it, all features are selected!!!")
+  ## keep track of the auc value for each important feature
+  impVars.auc.mean = c()
+  impVars.auc.std = c()
+  for(i in 1:length(impVars)){
+    ## add a new feature to the existing feature list
+    ftr = c(ftrSelected, impVars[i])
+    ftr.idx = which(names(trn) %in% ftr)
+    if(length(ftr) != length(ftr.idx))
+      stop("length(ftr) doesn't equal to length(ftr.idx).")
+    ## store auc values for each folder data set
+    score = c()
+    for(j in 1:R){
+      training = trn[trn.idx[[j]], ftr.idx, with=FALSE]
+      training.y = trn.y[trn.idx[[j]]]
+      testing = trn[-trn.idx[[j]], ftr.idx, with=FALSE]
+      testing.y = trn.y[-trn.idx[[j]]]
+      
+      dtrain <- xgb.DMatrix(data=Matrix(as.matrix(training), sparse = TRUE), label=training.y, missing = -999)
+      dtest <- xgb.DMatrix(data=Matrix(as.matrix(testing), sparse = TRUE), label=testing.y, missing = -999)
+      watchlist <- list(test=dtest)
+      
+      print("Train xgboost using xgb.train with watchlist")
+      params = list(
+        objective = "binary:logistic",
+        eta = optpar$eta, 
+        max_depth = optpar$Depth, 
+        subsample = optpar$r_sample,
+        eval_metric = "auc"
+      )
+      bst <- xgb.train(params = params,
+                       data=dtrain,
+                       nrounds = 2000,
+                       watchlist=watchlist,
+                       verbose = 0,
+                       early.stop.round = 20)
+      score = c(score, bst$bestScore)
+    }
+    impVars.auc.mean[i] = mean(score)
+    impVars.auc.std[i] = sd(score)
   }
-  if(length(score) == R)
-    res[i,] = c(ftr, score)
-  write.csv(res, paste0("tuning/TAE_feature_", m, ".csv"), row.names = FALSE)
+  idx = which.max(impVars.auc.mean)
+  cat(c(impVars[idx], impVars.auc.mean[idx], impVars.auc.std[idx]), sep=",")
+  
+  ftrSelected = c(ftrSelected, impVars[idx])
+  aucMean = c(aucMean, impVars.auc.mean[idx])
+  aucStd = c(aucStd, impVars.auc.std[idx])
+  res = data.table(ftrSelected, aucMean, aucStd)
+  # res = rbindlist(list(res, list(impVars[idx], impVars.auc.mean[idx], impVars.auc.std[idx])))
+  write.csv(res, paste0("tuning/hexiehao_R", R, ".csv"), row.names = FALSE)
+  if(k > 10){
+    last10 = res$aucMean[(k-9):k]
+    if(sum(sign(diff(last10))) < 0) 
+      break
+  }
 }
 
